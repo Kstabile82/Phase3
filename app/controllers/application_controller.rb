6d0dade9affafeebbb9629e3f5cf9c8f42eff51d
KarinaStabile <karina.stabile@gmail.com>
2022-05-15 01:01:00 -0400

    Added patch methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,7 @@ class ApplicationController < Sinatra::Base
 #       :volunteers
 #     ])
 #  end
+
 get '/rescues/:id' do
   resq = Rescue.find(params[:id])
    resq.to_json(include: [ 
@@ -41,6 +42,31 @@ end
   ra.to_json
  end
 
+ get '/rescues/:id/volunteers/:id' do
+  resq = Rescue.find(params[:id])
+  rv = resq.volunteers
+  resqvol = rv.find(params[:id])
+  resqvol.to_json
+ end
+
+ get '/rescues/:id/animals/:id' do
+  resq = Rescue.find(params[:id])
+  ra = resq.animals
+  resqan = ra.find(params[:id])
+  resquan.to_json
+ end
+
+ patch '/rescues/:id/animals/:id' do
+  resq = Rescue.find(params[:id])
+  ra = resq.animals
+  ra.to_json
+ end
+
+ post "/rescues" do
+  newRescue = Rescue.create(name: params[:name], location: params[:location])
+  newRescue.to_json
+ end
+ 
  delete "/rescues/:id/animals" do
   anim = Animal.find(params[:id])
   anim.destroy
@@ -57,6 +83,12 @@ post "/rescues/:id/volunteers" do
   vol = Volunteer.create(location: params[:location], name: params[:name], talents: params[:talents])
   vol.to_json
 end
+
+patch '/rescues/:id/volunteers/:id' do
+  resq = Rescue.find(params[:id])
+  rv = resq.volunteers
+  rv.to_json
+ end
 
 post "/rescues/:id/animals" do
   anim = Animal.create(age: params[:age], name: params[:name], sex: params[:sex], breed: params[:breed], kind: params[:kind], color: params[:color], adoption_status: params[:adoption_status])
