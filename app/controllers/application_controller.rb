6e4e4bf268261ce9fa78de6c406217f56bfb7e86
KarinaStabile <karina.stabile@gmail.com>
2022-06-09 22:21:35 -0400

    Updated ProjectVolunteers post requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,9 +38,8 @@ end
  get '/rescues/:id/projects' do
   resq = Rescue.find(params[:id])
   rp = resq.projects
-  rp.to_json(include: [ :project_volunteers, :project_animals ])
-  # rp.to_json(include: :project_volunteers)
-
+  rp.to_json(include: [ :project_volunteers, :project_animals, :volunteers ])
+  # rp.to_json(include: :project_volunteers) //get rid of Project-volunteers
  end
 
  get '/animals/:id' do
@@ -50,12 +49,18 @@ end
 
  get '/projects/:id' do 
   proj = Project.find(params[:id])
-  proj.to_json(include: [ :project_volunteers, :project_animals ])
+  proj.to_json(include: [ :project_volunteers, :project_animals, volunteers ])
+ end
+
+ get '/projects/:id/project_volunteers' do 
+  proj = Project.find(params[:id])
+  pv = proj.project_volunteers
+  pv.to_json
  end
 
  get '/volunteers/:id' do
   vol = Volunteer.find(params[:id])
-  vol.to_json
+  vol.to_json(include: [ :project_volunteers, :projects ])
  end
 
  get '/rescues/:id/volunteers/:volunteer_id' do
@@ -65,7 +70,34 @@ end
  
  get '/rescues/:id/projects/:project_id' do
   resq = Rescue.find(params[:id]).projects.find(params[:project_id])
+  resq.to_json(include: [ :project_volunteers, :project_animals ])
+ end
+
+ get '/rescues/:id/projects/:project_id/project_volunteers' do
+  resq = Rescue.find(params[:id]).projects.find(params[:project_id]).project_volunteers
   resq.to_json
+ end
+
+ get '/project_animals' do 
+  ProjectAnimals.all.to_json
+ end
+
+ get '/project_volunteerss' do 
+  ProjectVolunteer.all.to_json
+ end
+
+#  get '/project_volunteers' do 
+#   ProjectVolunteers.all.to_json
+#  end
+
+ post '/project_animals' do
+  projAn = ProjectAnimal.create(project_id: params[:project_id], animal_id: params[:animal_id])
+  projAn.to_json
+ end
+
+ post '/project_volunteers' do
+  projVol = ProjectVolunteer.create(project_id: params[:project_id], volunteer_id: params[:volunteer_id])
+  projVol.to_json
  end
 
 post '/animals' do
@@ -118,5 +150,12 @@ patch '/volunteers/:id' do
   vol.destroy
   vol.to_json
  end
+
+ delete '/project_volunteers/:id' do
+  projVol = ProjectVolunteer.find(params[:id])
+  projVol.destroy
+  projVol.to_json
+ end
+
 
 end
